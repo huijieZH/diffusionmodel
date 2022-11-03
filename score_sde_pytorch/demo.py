06067cd36edb0e6efaa37a43b047b8e8f625a24e
370620267@qq.com <huijiezh@umich.edu>
2022-11-03 18:17:24 -0400

    update

---
+++ score_sde_pytorch/demo.py
@@ -0,0 +1,110 @@
+from dataclasses import dataclass, field
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import torch
+from losses import get_optimizer
+from models.ema import ExponentialMovingAverage
+
+import torch.nn as nn
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import tensorflow_gan as tfgan
+import likelihood
+import controllable_generation
+from utils import restore_checkpoint
+sns.set(font_scale=2)
+sns.set(style="whitegrid")
+
+import models
+from models import utils as mutils
+from models import ncsnv2
+from models import ncsnpp
+from models import ddpm as ddpm_model
+from models import layerspp
+from models import layers
+from models import normalization
+import sampling
+from likelihood import get_likelihood_fn
+from sde_lib import VESDE, VPSDE, subVPSDE
+from sampling import (ReverseDiffusionPredictor, 
+                      LangevinCorrector, 
+                      EulerMaruyamaPredictor, 
+                      AncestralSamplingPredictor, 
+                      NoneCorrector, 
+                      NonePredictor,
+                      AnnealedLangevinDynamics)
+import datasets
+sde = 'VESDE' #@param ['VESDE', 'VPSDE', 'subVPSDE'] {"type": "string"}
+if sde.lower() == 'vesde':
+  from configs.ve import cifar10_ncsnpp_continuous as configs
+  ckpt_filename = "exp/ve/cifar10_ncsnpp_continuous/checkpoint_24.pth"
+  config = configs.get_config()  
+  sde = VESDE(sigma_min=config.model.sigma_min, sigma_max=config.model.sigma_max, N=config.model.num_scales)
+  sampling_eps = 1e-5
+elif sde.lower() == 'vpsde':
+  from configs.vp import cifar10_ddpmpp_continuous as configs  
+  ckpt_filename = "exp/vp/cifar10_ddpmpp_continuous/checkpoint_8.pth"
+  config = configs.get_config()
+  sde = VPSDE(beta_min=config.model.beta_min, beta_max=config.model.beta_max, N=config.model.num_scales)
+  sampling_eps = 1e-3
+elif sde.lower() == 'subvpsde':
+  from configs.subvp import cifar10_ddpmpp_continuous as configs
+  ckpt_filename = "exp/subvp/cifar10_ddpmpp_continuous/checkpoint_26.pth"
+  config = configs.get_config()
+  sde = subVPSDE(beta_min=config.model.beta_min, beta_max=config.model.beta_max, N=config.model.num_scales)
+  sampling_eps = 1e-3
+
+batch_size =   64#@param {"type":"integer"}
+config.training.batch_size = batch_size
+config.eval.batch_size = batch_size
+
+random_seed = 0 #@param {"type": "integer"}
+
+sigmas = mutils.get_sigmas(config)
+scaler = datasets.get_data_scaler(config)
+inverse_scaler = datasets.get_data_inverse_scaler(config)
+score_model = mutils.create_model(config)
+
+optimizer = get_optimizer(config, score_model.parameters())
+ema = ExponentialMovingAverage(score_model.parameters(),
+                               decay=config.model.ema_rate)
+state = dict(step=0, optimizer=optimizer,
+             model=score_model, ema=ema)
+
+state = restore_checkpoint(ckpt_filename, state, config.device)
+ema.copy_to(score_model.parameters())
+img_size = config.data.image_size
+channels = config.data.num_channels
+shape = (batch_size, channels, img_size, img_size)
+predictor = ReverseDiffusionPredictor #@param ["EulerMaruyamaPredictor", "AncestralSamplingPredictor", "ReverseDiffusionPredictor", "None"] {"type": "raw"}
+corrector = LangevinCorrector #@param ["LangevinCorrector", "AnnealedLangevinDynamics", "None"] {"type": "raw"}
+snr = 0.16 #@param {"type": "number"}
+n_steps =  1#@param {"type": "integer"}
+probability_flow = False #@param {"type": "boolean"}
+sampling_fn = sampling.get_pc_sampler(sde, shape, predictor, corrector,
+                                      inverse_scaler, snr, n_steps=n_steps,
+                                      probability_flow=probability_flow,
+                                      continuous=config.training.continuous,
+                                      eps=sampling_eps, device=config.device)
+
+def image_grid(x):
+  size = config.data.image_size
+  channels = config.data.num_channels
+  img = x.reshape(-1, size, size, channels)
+  w = int(np.sqrt(img.shape[0]))
+  img = img.reshape((w, w, size, size, channels)).transpose((0, 2, 1, 3, 4)).reshape((w * size, w * size, channels))
+  return img
+
+def show_samples(x):
+  x = x.permute(0, 2, 3, 1).detach().cpu().numpy()
+  img = image_grid(x)
+  plt.figure(figsize=(8,8))
+  plt.axis('off')
+  plt.imshow(img)
+  plt.show()
+
+x, n = sampling_fn(score_model)
+print("finish")
